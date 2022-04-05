ad60974259c4a98f1cda4f7706a506292b88676c
jeissonmoncada001 <jeissonmoncada001@gmail.com>
2022-04-05 15:51:53 -0500

    BR-61: Create project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>Timing Web</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
