895c4008f0cd588819ce41eded64e32955ccf979
jeissonmoncada001 <jeissonmoncada001@gmail.com>
2022-04-07 23:25:31 -0500

    BR-72 Add translations

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import React, { FC } from 'react';
+import React, { FC, Suspense } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './common/Home';
 
-const App: FC = () => <Home />;
+const App: FC = () => (
+  <Suspense fallback={null}>
+    <Router>
+      <Home />
+    </Router>
+  </Suspense>
+);
 
 export default App;
--- src/common/Home.tsx
+++ src/common/Home.tsx
@@ -1,6 +1,15 @@
 import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
 import Header from './components/Header';
 
-const Home: FC = () => <Header />;
+const Home: FC = () => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <Header />
+      <h1>{t('title')}</h1>
+    </>
+  );
+};
 
 export default Home;
