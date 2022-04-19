88c1f3c2d2117be44b76d08446f3bc33ff37be43
CrisDFL <93411477+CrisDFL@users.noreply.github.com>
2022-04-19 13:21:20 -0500

    BR-65: Create title (#8)
    * BR-65: Create title
    
    * BR-65: Create title
    
    * BR-65 - Fix styles
    
    * BR-65: Fix Conflics
    
    Co-authored-by: Julian Clavijo <98969962+JulianClavijo9411@users.noreply.github.com>
    Co-authored-by: jeissonmoncada001 <jeissonmoncada001@gmail.com>

--- src/common/Home.tsx
+++ src/common/Home.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 import EntryPage from './components/Entry';
 import Header from './components/Header';
+import Title from './components/Title';
 
 const Home: FC = () => (
   <>
     <Header />
+    <Title />
     <EntryPage />
   </>
 );
