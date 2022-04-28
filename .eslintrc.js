49b17387eda04e8f1b60da4e64146a93be4a54ab
jeissonmoncada001 <67172913+jeissonmoncada001@users.noreply.github.com>
2022-04-28 13:49:25 -0500

    BR-68: Add configuration modal (#11)
    * BR-68: Add configuration modal
    
    * BR-68:  Add configuration modal
    
    Co-authored-by: CrisDFL <93411477+CrisDFL@users.noreply.github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -58,6 +58,7 @@ module.exports = {
       { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
     ],
     'react/react-in-jsx-scope': 'off',
+    'linebreak-style': ['error', 'windows'],
   },
   settings: {
     'import/resolver': {
---
+++ src/common/components/Settings/index.tsx
@@ -0,0 +1,68 @@
+import { FC, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import Modal from '@mui/material/Modal';
+import './style.css';
+import { Button } from '@mui/material';
+import { ReactSVG } from 'react-svg';
+import { Entry } from '../../../types';
+
+const Settings: FC<{
+  entry: Entry;
+  setEntry: (entry: Entry) => void;
+}> = ({ entry, setEntry }) => {
+  const { t } = useTranslation();
+  const [visible, setVisible] = useState(false);
+  const { ovulationDays, inseminationDays } = entry;
+
+  const handleOpen = () => setVisible(true);
+  const handleClose = () => setVisible(false);
+
+  const addOvulation = () => {
+    setEntry({ ...entry, ovulationDays: ovulationDays + 1 });
+  };
+  const removeOvulation = () => {
+    setEntry({ ...entry, ovulationDays: ovulationDays - 1 });
+  };
+  const addInsemination = () => {
+    setEntry({ ...entry, inseminationDays: inseminationDays + 1 });
+  };
+  const removeInsemination = () => {
+    setEntry({ ...entry, inseminationDays: inseminationDays - 1 });
+  };
+
+  return (
+    <div>
+      <Button variant="text" className="buttonAdd" onClick={handleOpen}>
+        <ReactSVG src="/resources/svg/setting.svg" className="settings" />
+        {t('settings')}
+      </Button>
+      <Modal open={visible} onClose={handleClose} className="modal">
+        <div className="modalContainer">
+          <p>{t('preferences')}</p>
+          <div className="containerCounter">
+            <h3 className="title">{t('afterOvulation')}</h3>
+            <Button onClick={removeOvulation}>
+              <ReactSVG src="/resources/svg/arrowLeft.svg" />
+            </Button>
+            <div className="days">{ovulationDays}</div>
+            <Button onClick={addOvulation}>
+              <ReactSVG src="/resources/svg/arrowRight.svg" />
+            </Button>
+          </div>
+          <div className="containerCounter">
+            <h3 className="title">{t('afterInsemination')}</h3>
+            <Button onClick={removeInsemination}>
+              <ReactSVG src="/resources/svg/arrowLeft.svg" />
+            </Button>
+            <div className="days">{inseminationDays}</div>
+            <Button onClick={addInsemination}>
+              <ReactSVG src="/resources/svg/arrowRight.svg" />
+            </Button>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default Settings;
