f5c1f177155fa16de813b5f8d43dc5f591f4e2e0
jeissonmoncada001 <67172913+jeissonmoncada001@users.noreply.github.com>
2022-05-12 14:16:35 -0500

    BR-74: Change name and Icon (#13)
    * BR-74: Change name and Icon
    
    * BR-74: Fix responsive styles
    
    * BR-74: Fix responsive styles result
    
    * BR-74: correction comments

--- src/common/components/Result/index.tsx
+++ src/common/components/Result/index.tsx
@@ -6,42 +6,53 @@ import Bar from '../ProgressBar';
 import './style.css';
 
 interface IResult {
-    progressValue: number;
-    counterDaysByOvulation: number;
-    rangeDates: string;
-    setCounterLeft: number;
-    probableDay: string;
+  progressValue: number;
+  counterDaysByOvulation: number;
+  rangeDates: string;
+  setCounterLeft: number;
+  probableDay: string;
 }
 
 const Result: FC<IResult> = ({
-  progressValue, counterDaysByOvulation, rangeDates, setCounterLeft, probableDay,
+  progressValue,
+  counterDaysByOvulation,
+  rangeDates,
+  setCounterLeft,
+  probableDay,
 }) => {
   const { t } = useTranslation();
 
   return (
-        <div className="result">
-            <div className="resultContainer">
-                <div className="date">
-                    <h4 className="possibleDay">{t('possibleDay')}:&nbsp;&nbsp;&nbsp;
-                        <h4 className="possible">{probableDay}</h4>
-                    </h4>
-                    <h4 className="rangeDate">{t('rangeDate')}:&nbsp;&nbsp;&nbsp;
-                        <h4 className="range">{rangeDates}</h4>
-                    </h4>
-                </div>
-                <div className="bar">
-                    <Bar
-                        completed={progressValue}
-                        daysLeft={setCounterLeft}
-                        customLabel={<>
-                            <div className="svg">
-                                <ReactSVG src="/resources/svg/primaryLogo.svg" />
-                            </div>
-                            <h4 className="day">{t('day')} {counterDaysByOvulation}</h4>
-                        </>} />
-                </div>
-            </div>
+    <div className="result">
+      <div className="resultContainer">
+        <div className="date">
+          <h4 className="sectionResult">
+            {t('possibleDay')}
+            <h4 className="possible">{probableDay}</h4>
+          </h4>
+          <h4 className="sectionResult">
+            {t('rangeDate')}
+            <h4 className="range">{rangeDates}</h4>
+          </h4>
         </div>
+        <div className="bar">
+          <Bar
+            completed={progressValue}
+            daysLeft={setCounterLeft}
+            customLabel={
+              <>
+                <div className="svg">
+                  <ReactSVG src="/resources/svg/primaryLogo.svg" />
+                </div>
+                <h4 className="day">
+                  {t('day')} {counterDaysByOvulation}
+                </h4>
+              </>
+            }
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
